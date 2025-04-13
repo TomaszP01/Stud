0b80a9ebd49116d0fd717d5d18fc1f514e96e209
TomaszP01 <tomaszpolit01@gmail.com>
2025-04-13 18:20:42 +0200

    Cleaned

--- BAW/web-app/web-app-frontend/src/components/ProtectedRoute.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Navigate, Outlet } from "react-router-dom";
-
-const ProtectedRoute = ({ isAuthenticated }) => {
-  if (isAuthenticated === null) return null; // Czekaj na sprawdzenie tokena
-  return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
-};
-
-export default ProtectedRoute;
